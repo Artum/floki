50cd67b20a08f170ce45cb0ee7dab85fac040cb7
Artum Tchachkin <aptem.tchachkin@gmail.com>
2020-10-02 18:13:29 +0300

    Added user profile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,22 +1,46 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    maxWidth: "1000",
   },
-  title: {
-    flexGrow: 1,
+  media: {
+    height: 200,
+  },
+  avatar: {
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+    margin: "auto",
+  },
+  details: {
+    margin: "auto",
+    marginLeft: 5,
   },
 }));
 
 export default function UserProfile() {
+  const userProfile = useSelector((state) => state.userAuthentication.userProfile);
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <h1>User Profile</h1>
-    </div>
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="https://picsum.photos/1000" />
+      <Avatar alt={userProfile.fullName} src={userProfile.imageUrl} className={classes.avatar} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2" align="center">
+          {userProfile.fullName}
+        </Typography>
+        <Typography gutterBottom variant="h6" align="center">
+          {userProfile.email}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
