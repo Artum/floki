00b796eb10ac817207057d82d24a99275f6aa1ab
Artum Tchachkin <aptem.tchachkin@gmail.com>
2021-09-08 20:56:56 +0300

    dashboard add grid and title

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,43 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   content: {
     alignItems: "stretch",
     justifyContent: "stretch",
-  }
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
 }));
 
 export default function Dashboard() {
   const classes = useStyles();
 
   return (
-    <div className={classes.content}>
-      <h1>Dashboard</h1>
-    </div>
+    <Grid container className={classes.root} spacing={2}>
+      <Typography variant="h1" component="h2" gutterBottom>
+        Dashboard
+      </Typography>
+      
+      <Grid item xs={12}>
+        <Grid container justifyContent="left" spacing={spacing}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
